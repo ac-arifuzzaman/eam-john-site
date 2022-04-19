3867a7bb933369b6f99fcd68b2c485c8015e6f87
arifuzzaman arif <ac.arifarifuzzaman1@gmail.com>
2022-04-19 09:15:35 +0600

    after pagination

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,11 +1,21 @@
-console.log(process.env);
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-};
+// console.log(process.env);
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+// };
 
+// export default firebaseConfig;
+const firebaseConfig = {
+  apiKey: "AIzaSyAYRCojCwtQdiAgzzeP5bEYThrT2GoJLMI",
+  authDomain: "simple-firebase-authenti-f45fe.firebaseapp.com",
+  projectId: "simple-firebase-authenti-f45fe",
+  storageBucket: "simple-firebase-authenti-f45fe.appspot.com",
+  messagingSenderId: "70698269837",
+  appId: "1:70698269837:web:b3af2bcbe2d7509e14f6c4",
+  measurementId: "G-T6XJHKFNSE",
+};
 export default firebaseConfig;
